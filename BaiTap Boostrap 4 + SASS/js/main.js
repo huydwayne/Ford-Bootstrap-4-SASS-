306a53444bafa50f6e0d4651c039874b8cc39a83
HuyDwayne <anhhhuydayne@gmail.com>
2019-04-29 20:06:22 +0700

    Upload day 1

--- BaiTap Boostrap 4 + SASS/js/main.js
+++ BaiTap Boostrap 4 + SASS/js/main.js
@@ -1,7 +1,18 @@
-function on(){
-    document.getElementById('overlay').style.display="block";
-}
+$(document).ready(function(){
+    // Turn on - Turn off overlay when click to dropdown
+    $("#btnDropDown,#overlay").click(function(){
+        if ($("#overlay").attr("style") == "display:none"){
+            on();
+        } else {
+            off();
+        }
+    })
+    
+    function on(){
+        $("#overlay").attr("style","display:block");
+    }
 
-function off(){
-    document.getElementById('overlay').style.display="none";
-}
+    function off(){
+        $("#overlay").attr("style","display:none");
+    }
+})
